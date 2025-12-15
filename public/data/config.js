eb5815439d689a8280d1d356d6b0294bf342acdf
Jakit Patel <jakitpatel@gmail.com>
2025-12-15 15:37:58 +0530

    Change url

--- public/data/config.js
+++ public/data/config.js
@@ -1,7 +1,7 @@
 window.__APP_CONFIG__ = {
   //API_CLIENT_URL: "http://172.30.3.133:5656", // 👈 change this when moving to prod
 
-  API_CLIENT_URL1: "https://devlinux01.nyc.ou.org:5656", // 👈 change this when moving to prod
+  API_CLIENT_URL1: "https://devlinux03.nyc.ou.org", // 👈 change this when moving to prod
   API_CLIENT_URL2: "https://devvm01.nyc.ou.org:5656", // 👈 change this when moving to prod
   API_CLIENT_URL3: "http://localhost:3001", // 👈 change this when moving to prod
 };
